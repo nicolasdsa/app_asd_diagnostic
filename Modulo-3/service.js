705dd1bbc920ee56f6e8b3abbfd4f287c214cd29
Nicolas Sosa Machado <nicolassosamachado25@gmail.com>
2022-04-26 10:21:15 -0300

    Módulo 3 Completado

---
+++ Modulo-3/service.js
@@ -0,0 +1,17 @@
+const {get} = require('axios')
+const URL = `http://swapi.dev/api/people`;
+
+async function obterPessoas (nome){
+  const url = `${URL}/?search=${nome}&format=json`;
+  const result = await get(url);
+  return result.data.results.map(mapearPessoas)
+}
+
+function mapearPessoas(item){
+  return {
+    nome: item.name,
+    peso: item.height
+  }
+}
+
+module.exports = {obterPessoas};
