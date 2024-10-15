89e7bdfdf0c11064f50968f8743578732f5b3dd9
nicolas <nicolassosamachado25@gmail.com>
2024-10-14 22:34:11 -0300

    removing testing games

--- lib/games/pixel_adventure/components/collision_block.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flame/components.dart';
-
-class CollisionBlock extends PositionComponent {
-  bool isPlatform;
-  // super is used to call the constructor of the parent class and pass the position and size parameters
-  CollisionBlock({position, size, this.isPlatform = false})
-      : super(position: position, size: size);
-}
